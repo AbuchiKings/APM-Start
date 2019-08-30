209916eb9faf141586837795c51af413437b151c
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-08-30 23:26:59 +0100

    Using guards on routes

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product';
+import { ProductService } from './product.service';
+
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,11 +11,32 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  errorMessage = '';
+  product: IProduct | undefined;
 
-  constructor() { }
+  
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {
+  }
 
   ngOnInit() {
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
